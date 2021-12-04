e4ab8525f686389f944ed7d70db40c1ee96a6d0c
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-12-04 12:45:51 -0600

    Inputs para formulario - formik,Form,usefield

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { Formik, Form } from 'formik';
 import Container from "./components/Container";
+import Input from './components/Input';
 import Section from "./components/Section";
 
+const initialState = {
+  deposit: '',
+  contribution: '',
+  years: '',
+  rate: ''
+};
+
 function App() {
+
+  const handleSubmit = () => {
+    console.log('object')
+  };
+
   return (
     <Container>
-      <Section>Calculadora</Section>
+      <Section>
+        <Formik
+          initialValues={initialState}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input
+              name='deposit'
+              label='Depósito inicial'
+            />
+
+            <Input
+              name='contribution'
+              label='Contribución anual'
+            />
+
+            <Input
+              name='years'
+              label='Años'
+            />
+
+            <Input
+              name='rate'
+              label='Interés estimado'
+            />
+          </Form>
+        </Formik>
+      </Section>
     </Container>
   );
 }
