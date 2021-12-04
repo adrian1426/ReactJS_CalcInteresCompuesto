8d52314d4a1f4d84128c91441dca1233dd103c98
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-12-04 12:09:49 -0600

    Componente section, grid gris

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Container from "./components/Container";
+import Section from "./components/Section";
 
 function App() {
   return (
     <Container>
-      Calculadora
+      <Section>Calculadora</Section>
     </Container>
   );
 }
